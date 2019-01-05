767a6d51e4e993bc832f73e35c55cd676d1265f4
kevin <kevin7616@gmail.com>
2019-01-05 23:56:39 +1300

    added the stack and moving function

--- Assets/Script/TheStack.cs
+++ Assets/Script/TheStack.cs
@@ -4,13 +4,60 @@ using UnityEngine;
 
 public class TheStack : MonoBehaviour {
 
+	private const float BOUNDS_SIZE = 3.5f;
+
+	private float tileTransistion = 0.0f;
+	private float tileSpeed = 2.5f;
+
+	private GameObject[] theStack;
+	private int stackIndex;
+	private int scoreCount = 0;
 	// Use this for initialization
-	void Start () {
-		
+	private void Start () {
+		theStack = new GameObject[transform.childCount];
+		for (int i = 0; i < transform.childCount; i++) {
+			theStack [i] = transform.GetChild (i).gameObject;
+		}
+
+		stackIndex = transform.childCount - 1;
+			
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	private void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			if(placeTile()){
+				spawnTile ();
+				scoreCount++;
+			} else{
+				endGame();
+			}
+		}
+
+		moveTile ();
 	}
+
+	private void moveTile(){
+		tileTransistion += Time.deltaTime * tileSpeed;
+		theStack [stackIndex].transform.localPosition = new Vector3 (Mathf.Sin (tileTransistion) * BOUNDS_SIZE, scoreCount, 0);
+	}
+
+	private void spawnTile(){
+		stackIndex--;
+		if (stackIndex < 0)
+			stackIndex = transform.childCount - 1;
+		theStack [stackIndex].transform.localPosition = new Vector3 (0, scoreCount, 0);
+	}
+
+	private bool placeTile(){
+		return true;
+			
+	}
+
+	private void endGame(){
+
+	}
+	
+
+					
 }
